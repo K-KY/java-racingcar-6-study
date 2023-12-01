b800d56d07944a9b95d1f37206fa50eb7457b337
K-KY <kyuyoungk@naver.com>
2023-12-01 15:59:21 +0900

    Feat(InputView) : 자동차 이름을 입력받고 검증 후 리스트로 리턴

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,23 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.model.InputValidator;
+
+public class InputView {
+    private InputValidator inputValidator = new InputValidator();
+    public List<String> participants() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = Console.readLine();
+        return validateCarName(input);
+    }
+
+    private List<String> validateCarName(String input) {
+        List<String> carNames = List.of(input.split(","));
+        carNames.forEach(carName -> {
+            inputValidator.invalidCarName(carName);
+            inputValidator.nameLength(carName);
+        });
+        return carNames;
+    }
+}
