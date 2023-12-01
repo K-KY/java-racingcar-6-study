cf0138a93327bd04902d08ba41d53cbe999f33f7
K-KY <kyuyoungk@naver.com>
2023-12-01 16:14:54 +0900

    Fix(InputView) :접근 제한자 public 으로 변경

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -21,7 +21,7 @@ public class InputView {
         return carNames;
     }
 
-    private int trialCount() {
+    public int trialCount() {
         System.out.println("시도할 회수는 몇회인가요?");
         String input = Console.readLine();
         inputValidator.trialCount(input);
